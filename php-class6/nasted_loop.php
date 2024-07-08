0a84d0a534283642785836c08a6de0a5ac8291f6
omarfaruk477 <omarfaruk477@gmail.com>
2024-07-08 22:14:00 +0600

    nasted loop added

--- php-class6/nasted_loop.php
+++ php-class6/nasted_loop.php
@@ -1,9 +1,28 @@
 <?php
 
+
+
 // Nasted Loop
 
 for($i=1; $i<=20; $i++){
+
+
+    if($i%7==0 && $i%14==0){
+
+        //This is nasted loop
+        for($j=$i; $j>=0; $j--){
+            echo "Omar Faruk $j <br>";
+        }
+
+
+    }
+
+
     echo "$i <br>";
 }
+
+
+
+
 
 ?>
