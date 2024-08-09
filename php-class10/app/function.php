119a33552b4c89b8a25a758fac50cf7cc9f9e017
omarfaruk477 <omarfaruk477@gmail.com>
2024-08-09 20:11:34 +0600

    info function

--- php-class10/app/function.php
+++ php-class10/app/function.php
@@ -1,8 +1,32 @@
 <?php
 
+
+/**
+ * @param $name
+ * @param $age
+ * @param $skill
+ */
+
+function info( string $name, int $age, string $skill ){
+
+    if ( $skill ){
+        echo "Hello {$name}, You are {$age} years old and Your are a {$skill} developer.";
+    } else{
+        echo "Hello {$name}, You are {$age} years old.";
+    }
+
+}
+
+
+
+/*
 function creatTitle(string $title, string $tag = 'h1', string $align = 'center', string $fontSize = '50px', string $bg="red" ){
     echo "<{$tag} style='text-align:{$align}; font-size:{$fontSize}; background-color: {$bg};'>{$title}</{$tag}>";
 }
+*/
+
+
+
 
 ?>
 
--- php-class10/index.php
+++ php-class10/index.php
@@ -4,6 +4,12 @@ if( file_exists( __DIR__.'/autoload.php' )){
     require_once __DIR__.'/autoload.php';
 }
 
-creatTitle("PHP Heading", "h2", "center", "100px", "blue");
+
     
+    info ("Omer Faruk", 32 );
+
+/*
+creatTitle("PHP Heading", "h2", "center", "100px", "blue");
+*/
+
 ?>
