6d939014308ec25c910a57730ca0f5cb08de3be2
omarfaruk477 <omarfaruk477@gmail.com>
2024-08-10 13:07:28 +0600

    some update

--- Assignment/A_21_PasswordGenerator.php
+++ Assignment/A_21_PasswordGenerator.php
@@ -1,17 +1,10 @@
 <?php
 
-$textCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-$textSmall = "abcdefghijklmnopqrstuvwxyz";
-$numbers = "0123456789";
-$specialCharector = "@#$%^&*?";
+if( file_exists( __DIR__.'/autoload.php' )){
+    require_once __DIR__.'/autoload.php';
+}
 
-
-$capitalData = substr(str_shuffle($textCapital), 5, 2 );
-$smallData = substr(str_shuffle($textSmall), 9, 2 );
-$numbersData = substr(str_shuffle($numbers), 5, 1 );
-$specialCharectorData = substr(str_shuffle($specialCharector), 5, 1 );
-
-echo str_shuffle($capitalData.$smallData.$numbersData.$specialCharectorData);
+creatOtp()
 
 
 ?>
--- Assignment/app/function.php
+++ Assignment/app/function.php
@@ -1,6 +1,32 @@
 <?php
 
 /**
+ * @param $password generator
+ */
+function creatOtp(){
+
+    $textCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $textSmall = "abcdefghijklmnopqrstuvwxyz";
+    $numbers = "0123456789";
+    $specialCharector = "@#$%^&*?";
+    
+    $textCapitalShuffle = str_shuffle($textCapital);
+    $textSmallShuffle = str_shuffle($textSmall);
+    $numbersShuffle = str_shuffle($numbers);
+    $specialCharectorShuffle = str_shuffle($specialCharector);
+
+    $textCapitalData = substr($textCapitalShuffle, 4, 2);
+    $textSmallData = substr($textSmallShuffle, 8, 2);
+    $numbersData = substr($numbersShuffle, 4, 1);
+    $specialCharectorData = substr($specialCharectorShuffle, 2, 1);
+    
+    echo str_shuffle($textCapitalData.$textSmallData.$numbersData.$specialCharectorData);
+
+}
+
+
+
+/**
  * @param $elementor_header
  * @param $title
  * @param $tag
@@ -12,6 +38,7 @@
 function creatTitle(string $title, $tag = "h1", string $align = "center", string $fontsize = "100px", string $bg = "tomato"){
     echo "<{$tag} style=\"text-align:{$align}; font-size:{$fontsize}; background-color:{$bg};\">{$title}</{$tag}>";
 }
+
 
 
 
